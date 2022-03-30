ba8260b2ff945af791847d0dd48b6c26972596a0
Yida Niu <niuyida@bigai.ai>
2022-03-30 13:39:55 +0800

    refactor(compatibility): updated construct_vkc

--- src/vkc/include/vkc/construct_vkc.h
+++ src/vkc/include/vkc/construct_vkc.h
@@ -6,63 +6,63 @@
 #include <tesseract_common/resource_locator.h>
 #include <tesseract_srdf/srdf_model.h>
 #include <tesseract_environment/environment.h>
-#include <tesseract/tesseract.h>
+#include <tesseract_common/types.h>
+#include <tesseract_monitoring/environment_monitor.h>
 
 namespace vkc
 {
-class ConstructVKC
-{
-public:
-  using Ptr = std::shared_ptr<ConstructVKC>;
-  using ConstPtr = std::shared_ptr<const ConstructVKC>;
+  class ConstructVKC
+  {
+  public:
+    using Ptr = std::shared_ptr<ConstructVKC>;
+    using ConstPtr = std::shared_ptr<const ConstructVKC>;
 
-  // Constructor and destructor
-  ConstructVKC();
-  virtual ~ConstructVKC() = default;
+    // Constructor and destructor
+    ConstructVKC();
+    virtual ~ConstructVKC() = default;
 
-  // Load URDF and SRDF file to scene graph
-  bool loadURDFtoSceneGraph(const std::string& urdf_xml_file, const std::string& srdf_xml_file,
-                            tesseract_common::ResourceLocator::Ptr& locator);
+    // Load URDF and SRDF file to scene graph
+    bool loadURDFtoSceneGraph(const std::string &urdf_xml_file, const std::string &srdf_xml_file,
+                              tesseract_common::ResourceLocator::Ptr &locator);
 
+    // Initialize a tesseract environment that capatible to optimization framework;
+    bool initTesseract();
 
-  // Initialize a tesseract environment that capatible to optimization framework;
-  bool initTesseract();
+    void updateSceneGraph(tesseract_scene_graph::SceneGraph::Ptr sg);
 
-  void updateSceneGraph(tesseract_scene_graph::SceneGraph::Ptr sg);
+    // Return private members
+    tesseract_monitoring::EnvironmentMonitor::Ptr getTesseract();
+    tesseract_scene_graph::SceneGraph::Ptr getSceneGraph();
+    tesseract_srdf::SRDFModel::Ptr getSRDFModel();
 
-  // Return private members
-  tesseract::Tesseract::Ptr getTesseract();
-  tesseract_scene_graph::SceneGraph::Ptr getSceneGraph();
-  tesseract_srdf::SRDFModel::Ptr getSRDFModel();
+    const tesseract_scene_graph::SceneGraph::ConstPtr &getTesseractSceneGraph();
+    const tesseract_environment::Environment &getTesseractEnvironment();
 
-  const tesseract_scene_graph::SceneGraph::ConstPtr& getTesseractSceneGraph();
-  const tesseract_environment::Environment::Ptr& getTesseractEnvironment();
+    // // Break scene graph and reconnect
+    // bool construct(const std::string end_effector_link_name, const AttachLocation attach_location);
 
-  // // Break scene graph and reconnect
-  // bool construct(const std::string end_effector_link_name, const AttachLocation attach_location);
+  private:
+    tesseract_scene_graph::SceneGraph::Ptr scene_graph_;
+    tesseract_srdf::SRDFModel::Ptr srdf_model_;
+    tesseract_monitoring::EnvironmentMonitor::Ptr monitor_;
 
-private:
-  tesseract_scene_graph::SceneGraph::Ptr scene_graph_;
-  tesseract_srdf::SRDFModel::Ptr srdf_model_;
-  tesseract::Tesseract::Ptr tesseract_;
+    // Copy a link and all its child links/joints to a new scene graph
+    // tesseract_scene_graph::SceneGraph::Ptr breakSceneGraphatJoint(const std::string& scene_graph_name,
+    //                                                               const std::string& link_name);
 
-  // Copy a link and all its child links/joints to a new scene graph
-  // tesseract_scene_graph::SceneGraph::Ptr breakSceneGraphatJoint(const std::string& scene_graph_name,
-  //                                                               const std::string& link_name);
+    // void breakSceneGraphatJointHelper(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
+    //                                   const std::string link_name, long unsigned int child_joint_num);
 
-  // void breakSceneGraphatJointHelper(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
-  //                                   const std::string link_name, long unsigned int child_joint_num);
+    // const std::string constructHelper_FindLinkToWorld(const std::string leaf_link_name);
 
-  // const std::string constructHelper_FindLinkToWorld(const std::string leaf_link_name);
+    // void constructHelper_InvConnect(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
+    //                                 const std::string end_effector_link);
 
-  // void constructHelper_InvConnect(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
-  //                                 const std::string end_effector_link);
+    void clear();
 
-  void clear();
+    void clearTesseract();
+  };
 
-  void clearTesseract();
-};
+} // namespace vkc
 
-}  // namespace vkc
-
-#endif  // VKC_CONSTRUCTVKC_H
+#endif // VKC_CONSTRUCTVKC_H
--- src/vkc/src/construct_vkc.cpp
+++ src/vkc/src/construct_vkc.cpp
@@ -4,10 +4,10 @@ TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <tesseract_collision/bullet/bullet_discrete_bvh_manager.h>
 // #include <tesseract_environment/kdl/kdl_env.h>
 #include <tesseract_kinematics/core/utils.h>
-#include <tesseract_kinematics/kdl/kdl_fwd_kin_chain_factory.h>
-#include <tesseract_kinematics/kdl/kdl_fwd_kin_tree_factory.h>
-#include <tesseract_kinematics/kdl/kdl_inv_kin_chain_lma_factory.h>
+#include <tesseract_kinematics/kdl/kdl_factories.h>
+#include <tesseract_kinematics/kdl/kdl_inv_kin_chain_lma.h>
 #include <tesseract_urdf/urdf_parser.h>
+#include <tesseract_srdf/utils.h>
 #include <ros/ros.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
@@ -15,242 +15,117 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 namespace vkc
 {
-ConstructVKC::ConstructVKC()
-{
-  ROS_INFO("Virtual Kinematic Chain constructor initialized...");
-  clear();
-}
-
-bool ConstructVKC::loadURDFtoSceneGraph(const std::string& urdf_xml_file, const std::string& srdf_xml_file,
-                                        tesseract_scene_graph::ResourceLocator::Ptr& locator)
-{
-  clear();
-
-  // Parse urdf string into Scene Graph
-  tesseract_scene_graph::SceneGraph::Ptr scene_graph = tesseract_urdf::parseURDFString(urdf_xml_file, locator);
-  if (scene_graph == nullptr)
+  ConstructVKC::ConstructVKC()
   {
-    ROS_ERROR("Failed to parse URDF file: %s.", urdf_xml_file.c_str());
-    return false;
+    ROS_INFO("Virtual Kinematic Chain constructor initialized...");
+    clear();
   }
-  ROS_INFO("The root of scene graph is: %s", scene_graph->getRoot().c_str());
 
-  if (srdf_xml_file == "NULL")
+  bool ConstructVKC::loadURDFtoSceneGraph(const std::string &urdf_xml_file, const std::string &srdf_xml_file,
+                                          tesseract_common::ResourceLocator::Ptr &locator)
   {
-    ROS_WARN("Empty SRDF file, ignore.");
+    clear();
+
+    // Parse urdf string into Scene Graph
+    tesseract_scene_graph::SceneGraph::Ptr scene_graph = tesseract_urdf::parseURDFString(urdf_xml_file, *locator);
+    if (scene_graph == nullptr)
+    {
+      ROS_ERROR("Failed to parse URDF file: %s.", urdf_xml_file.c_str());
+      return false;
+    }
+    ROS_INFO("The root of scene graph is: %s", scene_graph->getRoot().c_str());
+
+    if (srdf_xml_file == "NULL")
+    {
+      ROS_WARN("Empty SRDF file, ignore.");
+      scene_graph_ = scene_graph;
+      srdf_model_ = std::make_shared<tesseract_srdf::SRDFModel>();
+      return true;
+    }
+
+    // Parse srdf string into SRDF Model
+    tesseract_srdf::SRDFModel::Ptr srdf = std::make_shared<tesseract_srdf::SRDFModel>();
+    // throws if error occurs, no need for the if.
+    srdf->initString(*scene_graph, srdf_xml_file, *locator);
+    // if (!srdf->initString(*scene_graph, srdf_xml_file))
+    // {
+    //   ROS_ERROR("Failed to parse SRDF file: %s.", srdf_xml_file.c_str());
+    //   return false;
+    // }
+
+    // Add allowed collision matrix to scene graph
+    tesseract_srdf::processSRDFAllowedCollisions(*scene_graph, *srdf);
+
+    // addVirtualMobileBase(scene_graph, srdf);
+
     scene_graph_ = scene_graph;
-    srdf_model_ = std::make_shared<tesseract_scene_graph::SRDFModel>();
+    srdf_model_ = srdf;
     return true;
   }
 
-  // Parse srdf string into SRDF Model
-  tesseract_scene_graph::SRDFModel::Ptr srdf = std::make_shared<tesseract_scene_graph::SRDFModel>();
-  if (!srdf->initString(*scene_graph, srdf_xml_file))
+  void ConstructVKC::updateSceneGraph(tesseract_scene_graph::SceneGraph::Ptr sg)
   {
-    ROS_ERROR("Failed to parse SRDF file: %s.", srdf_xml_file.c_str());
-    return false;
+    scene_graph_ = sg;
   }
 
-  // Add allowed collision matrix to scene graph
-  tesseract_scene_graph::processSRDFAllowedCollisions(*scene_graph, *srdf);
-
-  // addVirtualMobileBase(scene_graph, srdf);
-
-  scene_graph_ = scene_graph;
-  srdf_model_ = srdf;
-  return true;
-}
-
-void ConstructVKC::updateSceneGraph(tesseract_scene_graph::SceneGraph::Ptr sg)
-{
-  scene_graph_ = sg;
-}
-
-bool ConstructVKC::initTesseract()
-{
-  if (tesseract_ == nullptr)
+  bool ConstructVKC::initTesseract()
   {
-    //ROS_WARN("Null pointer to tesseract!");
-    tesseract_ = std::make_shared<tesseract::Tesseract>();
-  }
-  if (scene_graph_ == nullptr || srdf_model_ == nullptr)
-  {
-    ROS_ERROR("Null scene graph or srdf model!");
-  }
-  if (!tesseract_->init(scene_graph_, srdf_model_))
-  {
-    ROS_INFO("Failed to initialize tesseract environment");
-    return false;
-  }
-  return true;
-}
-
-tesseract::Tesseract::Ptr ConstructVKC::getTesseract()
-{
-  if (tesseract_ == nullptr)
-  {
-    ROS_ERROR("Null tesseract!");
-  }
-  return tesseract_;
-}
-
-tesseract_scene_graph::SceneGraph::Ptr ConstructVKC::getSceneGraph()
-{
-  return scene_graph_;
-}
-
-tesseract_scene_graph::SRDFModel::Ptr ConstructVKC::getSRDFModel()
-{
-  return srdf_model_;
-}
-
-const tesseract_environment::Environment::Ptr& ConstructVKC::getTesseractEnvironment()
-{
-  return tesseract_->getEnvironment();
-}
-
-const tesseract_scene_graph::SceneGraph::ConstPtr& ConstructVKC::getTesseractSceneGraph()
-{
-  return tesseract_->getEnvironment()->getSceneGraph();
-}
-
-/*
-tesseract_scene_graph::SceneGraph::Ptr ConstructVKC::breakSceneGraphatJoint(const std::string& scene_graph_name,
-                                                                            const std::string& link_name)
-{
-  ROS_INFO("Breaking scene graph of link %s", link_name.c_str());
-  tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch(new tesseract_scene_graph::SceneGraph());
-  breakSceneGraphatJointHelper(scene_graph_branch, link_name, scene_graph_->getInboundJoints(link_name).size());
-  scene_graph_branch->setName(scene_graph_name);
-  scene_graph_branch->setRoot(link_name);
-  if (!scene_graph_branch->isTree())
-  {
-    ROS_ERROR("Scene graph branch is not a tree!");
-  }
-  return scene_graph_branch;
-}
-
-void ConstructVKC::breakSceneGraphatJointHelper(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
-                                                const std::string link_name, long unsigned int child_joint_num)
-{
-  ROS_INFO("Adding Link:      %s to scene graph.", link_name.c_str());
-  if (!scene_graph_branch->addLink(*(scene_graph_->getLink(link_name))))
-  {
-    ROS_ERROR("Error when adding Link %s to scene graph.", link_name.c_str());
-  }
-
-  if (child_joint_num > 0)
-  {
-    for (const auto child_joint : scene_graph_->getOutboundJoints(link_name))
+    if (monitor_ == nullptr)
     {
-      std::string child_link_name = scene_graph_->getTargetLink(child_joint->getName())->getName();
-      ROS_WARN("Child link %s has %i child joints", child_link_name.c_str(),
-               scene_graph_->getOutboundJoints(child_link_name).size());
-      breakSceneGraphatJointHelper(scene_graph_branch, child_link_name,
-                                   scene_graph_->getOutboundJoints(child_link_name).size());
-
-      ROS_INFO("Adding Joint:     %s to scene graph.", child_joint->getName().c_str());
-      scene_graph_branch->addJoint(*child_joint);
+      // ROS_WARN("Null pointer to tesseract!");
+      monitor_ = std::make_shared<tesseract_monitoring::EnvironmentMonitor>();
     }
-    return;
+    if (scene_graph_ == nullptr || srdf_model_ == nullptr)
+    {
+      ROS_ERROR("Null scene graph or srdf model!");
+    }
+    if (!monitor_->getEnvironment().init(*scene_graph_, srdf_model_)) // init(scene_graph_, srdf_model_))
+    {
+      ROS_INFO("Failed to initialize tesseract environment");
+      return false;
+    }
+    return true;
   }
-  else
+
+  tesseract_monitoring::EnvironmentMonitor::Ptr ConstructVKC::getTesseract()
   {
-    ROS_WARN("Reach terminal Link: %s...", link_name.c_str());
-    return;
+    if (monitor_ == nullptr)
+    {
+      ROS_ERROR("Null tesseract!");
+    }
+    return monitor_;
   }
-}
 
-bool ConstructVKC::construct(const std::string end_effector_link_name,
-                             const ConstructVKC::AttachLocation attach_location)
-{
-  clearTesseract();
-
-  if (!scene_graph_->isTree())
+  tesseract_scene_graph::SceneGraph::Ptr ConstructVKC::getSceneGraph()
   {
-    ROS_ERROR("Scene graph is not a tree!");
+    return scene_graph_;
   }
 
-  tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch =
-      breakSceneGraphatJoint("branch", constructHelper_FindLinkToWorld(attach_location.link_name_));
-
-  constructHelper_InvConnect(scene_graph_branch, end_effector_link_name);
-
-  return true;
-}
-
-const std::string ConstructVKC::constructHelper_FindLinkToWorld(const std::string leaf_link_name)
-{
-  std::string world_object_link = "";
-  if (scene_graph_->getRoot() == leaf_link_name)
+  tesseract_srdf::SRDFModel::Ptr ConstructVKC::getSRDFModel()
   {
-    ROS_ERROR("Cannot find world link, or the leaf link is not connected to the world!");
-    return "";
+    return srdf_model_;
   }
-  if (scene_graph_->getSourceLink((scene_graph_->getInboundJoints(leaf_link_name)[0]->getName()))->getName() == "worl"
-                                                                                                                "d")
+
+  const tesseract_environment::Environment &ConstructVKC::getTesseractEnvironment()
   {
-    return leaf_link_name;
+    return monitor_->getEnvironment();
   }
-  else
+
+  const tesseract_scene_graph::SceneGraph::ConstPtr &ConstructVKC::getTesseractSceneGraph()
   {
-    world_object_link = constructHelper_FindLinkToWorld(
-        scene_graph_->getSourceLink((scene_graph_->getInboundJoints(leaf_link_name)[0]->getName()))->getName());
+    return monitor_->getEnvironment().getSceneGraph();
   }
-  return world_object_link;
-}
 
-void ConstructVKC::constructHelper_InvConnect(tesseract_scene_graph::SceneGraph::Ptr scene_graph_branch,
-                                              const std::string end_effector_link)
-{
-  // Remove branch from main scene graph
-  if (scene_graph_->getLink(scene_graph_branch->getRoot()) == nullptr)
+  void ConstructVKC::clear()
   {
-    ROS_ERROR("Tried to remove link (%s) that does not exist", scene_graph_branch->getRoot().c_str());
-    return;
+    srdf_model_ = nullptr;
+    scene_graph_ = nullptr;
+    monitor_ = nullptr;
   }
 
-  std::vector<tesseract_scene_graph::Joint::ConstPtr> joints =
-      scene_graph_->getInboundJoints(scene_graph_branch->getRoot());
-  assert(joints.size() <= 1);
-
-  // get child link names to remove
-  std::vector<std::string> child_link_names = scene_graph_->getLinkChildrenNames(scene_graph_branch->getRoot());
-
-  scene_graph_->removeLink(scene_graph_branch->getRoot());
-
-  for (const auto& link_name : child_link_names)
+  void ConstructVKC::clearTesseract()
   {
-    scene_graph_->removeLink(link_name);
+    monitor_ = nullptr;
   }
 
-  if (!environment_->init(scene_graph_))
-  {
-    ROS_ERROR("Failed to initialize environment.");
-    return;
-  }
-
-  environment_const_ = environment_;
-  registerDefaultContactManagers();
-  registerDefaultFwdKinSolvers();
-  registerDefaultInvKinSolvers();
-  initialized_ = true;
-
-  initTesseract();
-  return;
-}
-*/
-
-void ConstructVKC::clear()
-{
-  srdf_model_ = nullptr;
-  scene_graph_ = nullptr;
-  tesseract_ = nullptr;
-}
-
-void ConstructVKC::clearTesseract()
-{
-  tesseract_ = nullptr;
-}
-
-}  // namespace vkc
+} // namespace vkc
